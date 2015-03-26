ca3d20f15c8cb7ebcd18ba1f21a7d04b6ec8ecb8
Pablo Gomez <popigg@gmail.com>
2015-03-26 22:36:14 +0100

    decouple routes, add static and remove cache

--- server.js
+++ server.js
@@ -1,31 +1,23 @@
 'use strict';
 
-
-
 var express = require('express');
 var swig 	= require('swig');
 
-var router = express.Router();
-
-var app     = express();
+var routes = require('./server/routes');
+var app    = express();
 
 app.engine('html', swig.renderFile);
 
 app.use(express.static( __dirname + '/bower_components', { maxAge: 31556926000 /* 1year */ } ));
-app.use(express.static( __dirname + '/app', { maxAge: 31556926000 /* 1year */ }));
+app.use(express.static( __dirname + '/client', { maxAge: 31556926000 /* 1year */ }));
 
 app.set('view engine', 'html');
 app.set('views', __dirname);
-app.set('view cache', true);
+app.set('view cache', false);
 
-swig.setDefaults({ cache: true });
+swig.setDefaults({ cache: false });
 
-app.get('/', function(req, res){
-    res.render('index', { });
-});
-
-
-
+routes(app);
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
